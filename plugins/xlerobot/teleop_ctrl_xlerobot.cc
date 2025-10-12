5e4faf8c1ee41208102e92fe2556f2e070a3a837
aarontung <aarontung@frodobots.com>
2025-10-12 23:41:13 +0800

    Add plugins for xlerobot

---
+++ plugins/xlerobot/teleop_ctrl_xlerobot.cc
@@ -0,0 +1,329 @@
+#include <zmq.hpp>
+#include <string>
+#include <iostream>
+#include <mutex>
+#include <atomic>
+#include <thread>
+#include <chrono>
+#include <map>
+#include <nlohmann/json.hpp>
+#include <teleop_ctrl_plugin.h>
+
+using json = nlohmann::json;
+
+class TeleopCtrlXLerobot : public TeleopCtrlPlugin {
+ public:
+  TeleopCtrlXLerobot();
+  void Invoke() override;
+  void Terminate() override;
+  void OnMessageReceived(const char* message) override;
+  void HandleKeyUpdate(const json& key_action,
+                       const std::string& key_name,
+	               const std::string& map_key,
+	               double delta);
+  void HandleKeyUpdate(const json& key_action,
+                       const std::string& key_name,
+		       int index,
+	               const std::string& map_key,
+                       double delta,
+		       bool without_delta = false);
+  void ProcessKeyboardAction(const json& action, std::string& action_string);
+  void ProcessGamepadAction(const json& action, std::string& action_string);
+
+ private:
+  void PullThread();
+  zmq::context_t context_;
+  zmq::socket_t socket_action_;
+  zmq::socket_t socket_observation_;
+  std::atomic<bool> is_running_ = false;
+  std::thread pull_thread_;
+  json action_;
+  json observation_;
+  std::mutex lock_;
+};
+
+TeleopCtrlXLerobot::TeleopCtrlXLerobot() : context_(1),
+                                 is_running_(false),
+                                 socket_action_(context_, zmq::socket_type::push),
+                                 socket_observation_(context_, zmq::socket_type::pull) {
+}
+
+void TeleopCtrlXLerobot::ProcessKeyboardAction(const json& action,
+    std::string& action_string) {
+  json keyboard_action = json::object();
+  if (action.contains("data")) {
+    auto data = action["data"];
+    for (auto const& [key, value] : data.items()) {
+      if (!(value.is_boolean() && value == false)) {
+        keyboard_action[key] = value;
+      }
+    }
+  }
+  action_string = keyboard_action.dump();
+}
+
+void TeleopCtrlXLerobot::ProcessGamepadAction(const json& action,
+    std::string& action_string) {
+  json keyboard_action = json::object();
+  if (action.contains("data")) {
+    auto data = action["data"];
+
+    int right_mode = 0;
+    int left_mode = 0;
+    // 0: base control
+    // 1: arm translation
+    // 2: arm rotation
+    // 3: head control
+
+    // right hand
+    if (data["buttons"][7].get<double>() > 0 && data["buttons"][2].get<double>() > 0) {
+      keyboard_action["p"] = true;
+    }
+
+    if (data["buttons"][5].get<double>() > 0 && data["buttons"][7].get<double>() > 0) {
+      right_mode = 2;
+    } else if (data["buttons"][5].get<double>() > 0) {
+      right_mode = 1;
+    } else if (data["buttons"][7].get<double>() > 0) {
+      right_mode = 3;
+    }
+
+    if (right_mode == 0) {
+
+      if (data["axes"][2].get<double>() > 0.75) {
+        keyboard_action["/"] = true;
+      }
+
+      if (data["axes"][2].get<double>() < -0.75) {
+        keyboard_action["."] = true;
+      }
+
+    } else if (right_mode == 1) {
+
+      if (data["axes"][3].get<double>() > 0.8) {
+        keyboard_action["j"] = true;
+      } else if (data["axes"][3].get<double>() < -0.8) {
+        keyboard_action["u"] = true;
+      } else if (data["axes"][2].get<double>() > 0.8) {
+        keyboard_action["k"] = true;
+      } else if (data["axes"][2].get<double>() < -0.8) {
+        keyboard_action["h"] = true;
+      }
+
+      // axis z
+      if (data["buttons"][3].get<double>() > 0) {
+        keyboard_action["y"] = true;
+      }
+ 
+      if (data["buttons"][0].get<double>() > 0) {
+        keyboard_action["i"] = true;
+      }
+
+    } else if (right_mode == 2) {
+
+       if (data["axes"][3].get<double>() > 0.8) {
+        keyboard_action["g"] = true;
+      } else if (data["axes"][3].get<double>() < -0.8) {
+        keyboard_action["t"] = true;
+      } else if (data["axes"][2].get<double>() > 0.8) {
+        keyboard_action["6"] = true;
+      } else if (data["axes"][2].get<double>() < -0.8) {
+        keyboard_action["5"] = true;
+      }
+    } else if (right_mode == 3) {
+
+      if (data["axes"][3].get<double>() > 0.8) {
+        keyboard_action["9"] = true;
+      } else if (data["axes"][3].get<double>() < -0.8) {
+        keyboard_action["0"] = true;
+      } else if (data["axes"][2].get<double>() > 0.8) {
+        keyboard_action["8"] = true;
+      } else if (data["axes"][2].get<double>() < -0.8) {
+        keyboard_action["7"] = true;
+      }
+    }
+
+    // right gripper
+    if (data["buttons"][1].get<double>() > 0) {
+      keyboard_action["n"] = true;
+    }
+
+    if (data["buttons"][2].get<double>() > 0) {
+      keyboard_action["b"] = true;
+    }
+
+    // left arm
+    if (data["buttons"][6].get<double>() > 0 && data["buttons"][15].get<double>() > 0) {
+      keyboard_action["c"] = true;
+    }
+
+    if (data["buttons"][4].get<double>() > 0 && data["buttons"][6].get<double>() > 0) {
+      left_mode = 2;
+    } else if (data["buttons"][4].get<double>() > 0) {
+      left_mode = 1;
+    }
+
+    if (left_mode == 0) {
+      if (data["axes"][1].get<double>() > 0.75) {
+        keyboard_action["arrowdown"] = true;
+      }
+
+      if (data["axes"][1].get<double>() < -0.75) {
+        keyboard_action["arrowup"] = true;
+      }
+
+      if (data["axes"][0].get<double>() > 0.75) {
+        keyboard_action["arrowright"] = true;
+      }
+
+      if (data["axes"][0].get<double>() < -0.75) {
+        keyboard_action["arrowleft"] = true;
+      }
+
+    } else if (left_mode == 1) {
+      if (data["axes"][1].get<double>() > 0.8) {
+        keyboard_action["s"] = true;
+      } else if (data["axes"][1].get<double>() < -0.8) {
+        keyboard_action["w"] = true;
+      } else if (data["axes"][0].get<double>() > 0.8) {
+        keyboard_action["d"] = true;
+      } else if (data["axes"][0].get<double>() < -0.8) {
+        keyboard_action["a"] = true;
+      }
+
+      // axis z
+      if (data["buttons"][12].get<double>() > 0) {
+        keyboard_action["q"] = true;
+      }
+ 
+      if (data["buttons"][13].get<double>() > 0) {
+        keyboard_action["e"] = true;
+      }
+
+    } else if (left_mode == 2) {
+
+       // pitch and roll
+       if (data["axes"][1].get<double>() > 0.8) {
+        keyboard_action["f"] = true;
+      } else if (data["axes"][1].get<double>() < -0.8) {
+        keyboard_action["r"] = true;
+      } else if (data["axes"][0].get<double>() > 0.8) {
+        keyboard_action["2"] = true;
+      } else if (data["axes"][0].get<double>() < -0.8) {
+        keyboard_action["1"] = true;
+      }
+    }
+
+    // left gripper
+    if (data["buttons"][15].get<double>() > 0) {
+      keyboard_action["x"] = true;
+    }
+
+    if (data["buttons"][14].get<double>() > 0) {
+      keyboard_action["z"] = true;
+    }
+
+  }
+  action_string = keyboard_action.dump();
+ // std::cout << action_string << std::endl;
+}
+
+void TeleopCtrlXLerobot::OnMessageReceived(const char* data) {
+
+  size_t len = std::strlen(data);
+  if (is_running_) {
+
+    {
+      std::lock_guard<std::mutex> lock(lock_); 
+      if (!action_.contains("arm_gripper.pos")) {
+        action_ = observation_;
+      }
+    }
+    std::string action_string;
+    json action = json::parse(data);
+    if (action.contains("type") && action["type"] == "keyboard") {
+      ProcessKeyboardAction(action, action_string);
+    } else if (action.contains("type") && action["type"] == "gamepad") {
+      ProcessGamepadAction(action, action_string);
+    } else {
+      action_string = std::string(data);
+    }
+
+    if (action_string.length() > 0) {
+      zmq::message_t message(action_string.c_str(), action_string.length());
+      socket_action_.send(message, zmq::send_flags::none);
+    }
+  }
+}
+
+void TeleopCtrlXLerobot::Terminate() {
+  is_running_ = false;
+  if (pull_thread_.joinable()) {
+    pull_thread_.join();
+  }
+  socket_action_.close();
+  socket_observation_.close();
+  context_.close();
+}
+
+void TeleopCtrlXLerobot::Invoke() {
+  socket_action_.connect("tcp://172.17.0.2:5558");
+  socket_observation_.connect("tcp://172.17.0.2:5556");
+  is_running_ = true;
+  printf("Connected\n");
+  pull_thread_ = std::thread(&TeleopCtrlXLerobot::PullThread, this);
+  while (is_running_) {
+    if (is_recording_) {
+      //std::cout << observation_ << std::endl;
+
+      std::vector<float> observation = {
+        observation_["arm_shoulder_pan.pos"],
+        observation_["arm_shoulder_lift.pos"],
+        observation_["arm_wrist_flex.pos"],
+        observation_["arm_elbow_flex.pos"],
+        observation_["arm_wrist_roll.pos"],
+        observation_["arm_gripper.pos"],
+        observation_["x.vel"],
+        observation_["y.vel"],
+        observation_["theta.vel"]
+      };
+
+      std::vector<float> action = {
+        action_["arm_shoulder_pan.pos"],
+        action_["arm_shoulder_lift.pos"],
+        action_["arm_wrist_flex.pos"],
+        action_["arm_elbow_flex.pos"],
+        action_["arm_wrist_roll.pos"],
+        action_["arm_gripper.pos"],
+        action_["x.vel"],
+        action_["y.vel"],
+        action_["theta.vel"]
+      };
+
+      IngestTelemetry(observation.data(), observation.size(), action.data(), action.size());
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+  }
+}
+
+void TeleopCtrlXLerobot::PullThread() {
+  while (is_running_) {
+    zmq::message_t msg;
+    zmq::recv_result_t result = socket_observation_.recv(msg, zmq::recv_flags::dontwait);
+    if (result) {
+      std::string received(static_cast<char*>(msg.data()), msg.size());
+
+      {
+        std::lock_guard<std::mutex> lock(lock_); 
+        observation_ = json::parse(received);
+      }
+
+      //std::cout << "observation: " << received << std::endl;
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  }
+}
+
+extern "C" TeleopCtrlPlugin* create_plugin() {
+  return new TeleopCtrlXLerobot();
+}
---
+++ plugins/xlerobot/teleop_media_xlerobot.cc
@@ -0,0 +1,244 @@
+#include <iostream>
+#include <thread>
+#include <gst/gst.h>
+#include <set>
+#include "teleop_media_plugin.h"
+
+const int kFrontCameraId = 1000001;
+const int kWristRightCameraId = 1100001;
+const int kWristLeftCameraId = 1200001;
+
+uint64_t get_timestamp_ms() {
+  struct timespec ts;
+  clock_gettime(CLOCK_REALTIME, &ts);
+  return (uint64_t)ts.tv_sec * 1000 + ts.tv_nsec / 1000000;
+}
+
+void set_bit_1x2_pixels(uint8_t *y_plane, int start_x, int start_y, bool bit) {
+  uint8_t val_on = 200;
+  uint8_t val_off = 16;
+
+  if (bit) {
+    y_plane[start_y * 640 + start_x] = val_off;
+    y_plane[start_y * 640 + start_x + 1] = val_on;
+  } else {
+    y_plane[start_y * 640 + start_x] = val_on;
+    y_plane[start_y * 640 + start_x + 1] = val_off;
+  }
+}
+
+void write_byte_1x2_pixels(uint8_t *y_plane, int start_x, int start_y, uint8_t byte) {
+  for (int i = 0; i < 8; ++i) {
+    bool bit = (byte >> (7 - i)) & 1;
+    set_bit_1x2_pixels(y_plane, start_x + i * 2, start_y, bit);
+  }
+}
+
+uint8_t calc_checksum(uint8_t *data, int len) {
+  uint8_t cs = 0;
+  for (int i = 0; i < len; i++) {
+    cs ^= data[i];
+  }
+  return cs;
+}
+
+void write_timestamp_and_checksum(uint8_t *y_plane, int start_y, uint64_t timestamp) {
+  uint8_t buf[9];
+  memcpy(buf, &timestamp, 8);
+
+  buf[8] = calc_checksum(buf, 8);
+
+  for (int i = 0; i < 9; i++) {
+    write_byte_1x2_pixels(y_plane, i * 16, start_y, buf[i]);
+  }
+}
+
+static void on_buffer_handoff(GstElement* identity, GstBuffer* buffer, GstPad* pad, gpointer user_data) {
+  GstMapInfo map;
+  if (!gst_buffer_map(buffer, &map, GST_MAP_WRITE)) return;
+
+  guint8* y_plane = map.data;
+  guint64 data = get_timestamp_ms();
+  write_timestamp_and_checksum(y_plane, 0, data); 
+  gst_buffer_unmap(buffer, &map);
+}
+
+class TeleopMediaGst : public TeleopMediaPlugin {
+ public:
+  TeleopMediaGst();
+  ~TeleopMediaGst();
+  void Invoke() override;
+  void Terminate() override;
+  void OnEvent(const char* event_name, const void* data) override;
+ private:
+  GstElement* pipeline1_;
+  GstElement* pipeline2_;
+  GstElement* pipeline3_;
+  GstElement* appsink1_;
+  GstElement* appsink2_;
+  GstElement* appsink3_;
+  GstElement* identity1_;
+  GstElement* identity2_;
+  GstElement* identity3_;
+  bool is_running_;
+  static GstFlowReturn OnVideoSampleMajor(GstElement* sink, gpointer user_data);
+  static GstFlowReturn OnVideoSampleWristRight(GstElement* sink, gpointer user_data);
+  static GstFlowReturn OnVideoSampleWristLeft(GstElement* sink, gpointer user_data);
+  std::set<int> user_ids_;
+};
+
+TeleopMediaGst::TeleopMediaGst() {
+  config_["camera1"] = "";
+  config_["camera2"] = "";
+  config_["camera3"] = "";
+}
+
+TeleopMediaGst::~TeleopMediaGst() {
+}
+
+GstFlowReturn TeleopMediaGst::OnVideoSampleMajor(GstElement* sink, gpointer user_data) {
+  TeleopMediaGst* self = static_cast<TeleopMediaGst*>(user_data);
+  GstSample* sample = NULL;
+  GstBuffer* buffer;
+  GstMapInfo info;
+  g_signal_emit_by_name(sink, "pull-sample", &sample);
+  if (sample) {
+    buffer = gst_sample_get_buffer(sample);
+    gst_buffer_map(buffer, &info, GST_MAP_READ);
+    //printf("send major video data, size: %d\n", info.size - 6);
+    self->SendVideoData(info.data + 6, info.size - 6);
+    gst_buffer_unmap(buffer, &info);
+    gst_sample_unref(sample);
+    return GST_FLOW_OK;
+  }
+  return GST_FLOW_ERROR;
+}
+
+GstFlowReturn TeleopMediaGst::OnVideoSampleWristRight(GstElement* sink, gpointer user_data) {
+  TeleopMediaGst* self = static_cast<TeleopMediaGst*>(user_data);
+  GstSample* sample = NULL;
+  GstBuffer* buffer;
+  GstMapInfo info;
+  g_signal_emit_by_name(sink, "pull-sample", &sample);
+  if (sample) {
+    buffer = gst_sample_get_buffer(sample);
+    gst_buffer_map(buffer, &info, GST_MAP_READ);
+    //printf("send minor video data, size: %d\n", info.size - 6);
+    self->SendVideoData(info.data + 6, info.size - 6, 1);
+    gst_buffer_unmap(buffer, &info);
+    gst_sample_unref(sample);
+    return GST_FLOW_OK;
+  }
+  return GST_FLOW_ERROR;
+}
+
+GstFlowReturn TeleopMediaGst::OnVideoSampleWristLeft(GstElement* sink, gpointer user_data) {
+  TeleopMediaGst* self = static_cast<TeleopMediaGst*>(user_data);
+  GstSample* sample = NULL;
+  GstBuffer* buffer;
+  GstMapInfo info;
+  g_signal_emit_by_name(sink, "pull-sample", &sample);
+  if (sample) {
+    buffer = gst_sample_get_buffer(sample);
+    gst_buffer_map(buffer, &info, GST_MAP_READ);
+    //printf("send minor video data, size: %d\n", info.size - 6);
+    self->SendVideoData(info.data + 6, info.size - 6, 2);
+    gst_buffer_unmap(buffer, &info);
+    gst_sample_unref(sample);
+    return GST_FLOW_OK;
+  }
+  return GST_FLOW_ERROR;
+}
+
+void TeleopMediaGst::OnEvent(const char* event_name, const void* data) {
+  if (strncmp(event_name, "video.user_joined", strlen("video.user_joined")) == 0) {
+    int user_id = *static_cast<const int*>(data);
+    if (user_id != kFrontCameraId && user_id != kWristRightCameraId && user_id != kWristLeftCameraId) {
+      user_ids_.insert(user_id);
+    }
+  } else if (strncmp(event_name, "video.user_offline", strlen("video.user_offline")) == 0) {
+    int user_id = *static_cast<const int*>(data);
+    if (user_id != kFrontCameraId && user_id != kWristRightCameraId && user_id != kWristLeftCameraId) {
+      user_ids_.erase(*static_cast<const int*>(data));
+    }
+  }
+
+  if (user_ids_.empty()) {
+    printf("No users online, stopping pipeline.\n");
+    gst_element_set_state(pipeline1_, GST_STATE_NULL);
+    gst_element_set_state(pipeline2_, GST_STATE_NULL);
+    gst_element_set_state(pipeline3_, GST_STATE_NULL);
+  } else {
+    printf("Users online: %zu, starting pipeline.\n", user_ids_.size());
+    gst_element_set_state(pipeline1_, GST_STATE_PLAYING);
+    gst_element_set_state(pipeline2_, GST_STATE_PLAYING);
+    gst_element_set_state(pipeline3_, GST_STATE_PLAYING);
+  }
+}
+
+
+void TeleopMediaGst::Invoke() {
+  gst_init(NULL, NULL);
+  is_running_ = true;
+  std::string encode_pipeine1 = " ! queue ! jpegdec ! videoconvert ! identity name=id ! queue ! x264enc tune=zerolatency speed-preset=ultrafast key-int-max=30 bitrate=512 bframes=0 ! appsink name=sink sync=false";
+  
+  std::string encode_pipeine2 = " ! queue ! jpegdec ! videoconvert ! identity name=id ! queue ! x264enc tune=zerolatency speed-preset=ultrafast key-int-max=30 bitrate=512 bframes=0 ! appsink name=sink sync=false";
+
+  std::string encode_pipeine3 = " ! queue ! jpegdec ! videoconvert ! identity name=id ! x264enc tune=zerolatency speed-preset=ultrafast key-int-max=30 bitrate=1024 bframes=0 ! appsink name=sink sync=false";
+
+  if (config_.count("camera1") && !config_["camera1"].empty()) {
+    std::string pipeline1 = config_["camera1"] + encode_pipeine1;
+    std::cout << pipeline1 << std::endl;
+    pipeline1_ = gst_parse_launch(pipeline1.c_str(), NULL);
+    appsink1_ = gst_bin_get_by_name(GST_BIN(pipeline1_), "sink");
+    identity1_ = gst_bin_get_by_name(GST_BIN(pipeline1_), "id");
+    //g_signal_connect(identity1_, "handoff", G_CALLBACK(on_buffer_handoff), NULL);
+    g_signal_connect(appsink1_, "new-sample", G_CALLBACK(OnVideoSampleWristRight), this);
+    g_object_set(appsink1_, "emit-signals", TRUE, NULL);
+  }
+
+
+  if (config_.count("camera2") && !config_["camera2"].empty()) {
+    std::string pipeline2 = config_["camera2"] + encode_pipeine2;
+    std::cout << pipeline2 << std::endl;
+    pipeline2_ = gst_parse_launch(pipeline2.c_str(), NULL);
+    appsink2_ = gst_bin_get_by_name(GST_BIN(pipeline2_), "sink");
+    identity2_ = gst_bin_get_by_name(GST_BIN(pipeline2_), "id");
+    //g_signal_connect(identity2_, "handoff", G_CALLBACK(on_buffer_handoff), NULL);
+    g_signal_connect(appsink2_, "new-sample", G_CALLBACK(OnVideoSampleWristLeft), this);
+    g_object_set(appsink2_, "emit-signals", TRUE, NULL);
+  }
+
+  if (config_.count("camera3") && !config_["camera3"].empty()) {
+    std::string pipeline3 = config_["camera3"] + encode_pipeine3;
+    std::cout << pipeline3 << std::endl;
+    pipeline3_ = gst_parse_launch(pipeline3.c_str(), NULL);
+    appsink3_ = gst_bin_get_by_name(GST_BIN(pipeline3_), "sink");
+    identity3_ = gst_bin_get_by_name(GST_BIN(pipeline3_), "id");
+    //g_signal_connect(identity3_, "handoff", G_CALLBACK(on_buffer_handoff), NULL);
+    g_signal_connect(appsink3_, "new-sample", G_CALLBACK(OnVideoSampleMajor), this);
+    g_object_set(appsink3_, "emit-signals", TRUE, NULL);
+  }
+
+  while (is_running_) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  }
+
+  gst_element_set_state(pipeline1_, GST_STATE_NULL);
+  gst_element_set_state(pipeline2_, GST_STATE_NULL);
+  gst_element_set_state(pipeline3_, GST_STATE_NULL);
+  gst_object_unref(pipeline1_);
+  gst_object_unref(pipeline2_);
+  gst_object_unref(pipeline3_);
+  gst_object_unref(appsink1_);
+  gst_object_unref(appsink2_);
+  gst_object_unref(appsink3_);
+}
+
+void TeleopMediaGst::Terminate() {
+  is_running_ = false;
+}
+
+extern "C" TeleopMediaPlugin* create_plugin() {
+  return new TeleopMediaGst();
+}
